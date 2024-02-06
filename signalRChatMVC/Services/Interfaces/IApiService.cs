c977e2c73a17c47b4baa79274e7f1643b17838a8
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-06 13:31:28 +0100

    Add IApiService interface

---
+++ signalRChatMVC/Services/Interfaces/IApiService.cs
@@ -0,0 +1,9 @@
+﻿namespace signalRChatMVC.Services.Interfaces;
+
+public interface IApiService
+{
+    Task<string> Login(string username,string password);
+    Task Register(string username, string firstname, string lastname, string password);
+    Task SendMessage(string sender, string roomName, string content, DateTime timestamp);
+    Task GetMessages(string roomId);
+}
