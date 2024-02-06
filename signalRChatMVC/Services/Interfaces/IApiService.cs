2a71bda32286e07ec38cd065849350026c7ba58b
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-06 13:54:44 +0100

    Update Register method to return bool

--- signalRChatMVC/Services/Interfaces/IApiService.cs
+++ signalRChatMVC/Services/Interfaces/IApiService.cs
@@ -3,7 +3,7 @@
 public interface IApiService
 {
     Task<string> Login(string username,string password);
-    Task Register(string username, string firstname, string lastname, string password);
+    Task<bool> Register(string username, string firstname, string lastname, string password,string email);
     Task SendMessage(string sender, string roomName, string content, DateTime timestamp);
     Task GetMessages(string roomId);
 }
