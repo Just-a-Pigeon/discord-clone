45497c6e0d534674e3780dd3dc8e9ed78d709659
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-06 13:55:38 +0100

    Add RegisterDto

---
+++ signalRChatMVC/DTOs/Auth/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace signalRChatMVC.DTOs.Auth;
+
+public class RegisterDto
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Username { get; set; }
+}
