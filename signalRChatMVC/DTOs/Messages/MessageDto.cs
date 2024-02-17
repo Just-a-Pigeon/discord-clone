9def24a5bbc8539be99af5b6f8465752c0dd59f6
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-17 16:20:09 +0100

    Add MessageDto

---
+++ signalRChatMVC/DTOs/Messages/MessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace signalRChatMVC.DTOs.Messages;
+
+public class MessageDto
+{
+    public string Sender { get; set; }
+    public string RoomName { get; set; }
+    public string Content { get; set; }
+    public DateTime Timestamp { get; set; }
+}
