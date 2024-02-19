7a9a6dafd18c4494db0385f751b9dbb9d776b311
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-19 15:45:43 +0100

    Some thing dll wise

--- signalRchat.Api/obj/Debug/net8.0/signalRchat.Api.AssemblyInfo.cs
+++ signalRchat.Api/obj/Debug/net8.0/signalRchat.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("signalRchat.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99e7a86df8e06a1eb4d002f32c62bc7c30628f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5792feccbae8981f1669286be19ac68203070933")]
 [assembly: System.Reflection.AssemblyProductAttribute("signalRchat.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("signalRchat.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
