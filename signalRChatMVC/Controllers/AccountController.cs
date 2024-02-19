99957fd4d6235ed01433b6bd4d48840d58f25a41
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2024-02-19 15:45:27 +0100

    FriendList method uses api service

--- signalRChatMVC/Controllers/AccountController.cs
+++ signalRChatMVC/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System.Net.Http.Headers;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
 using Newtonsoft.Json;
 using signalRChatMVC.Models;
+using signalRChatMVC.Services.Interfaces;
 using signalRChatMVC.ViewModels;
 
 namespace signalRChatMVC.Controllers
@@ -12,10 +14,12 @@ namespace signalRChatMVC.Controllers
     public class AccountController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IApiService _apiService;
 
-        public AccountController(IHttpClientFactory httpClientFactory)
+        public AccountController(IHttpClientFactory httpClientFactory, IApiService apiService)
         {
             _httpClientFactory = httpClientFactory;
+            _apiService = apiService;
         }
 
         // GET: AccountController
@@ -25,16 +29,13 @@ namespace signalRChatMVC.Controllers
                 return RedirectToAction("Login", "Auth");
             
             FriendListViewModel friendListViewModel = new FriendListViewModel();
-            var id = User.Claims.Where(c=> c.Type == ClaimTypes.NameIdentifier).First().Value;
-            var client = _httpClientFactory.CreateClient();
+            
+            var userId =  Guid.Parse(User.Claims.Where(c=> c.Type == ClaimTypes.NameIdentifier).First().Value);
             var token = HttpContext.Session.GetString("Token");
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            var apiEndpoint = $"http://localhost:5099/api/FriendRequest/friends/{id}"; // Replace with your API endpoint
             
-            
-            var response = await client.GetStringAsync(apiEndpoint);
-            
-            friendListViewModel.Friends =JsonConvert.DeserializeObject<List<UserModel>>(response);
+            var friendlist = await _apiService.GetFriends(userId, token);
+
+            friendListViewModel.Friends = friendlist;
             
             return View(friendListViewModel);
         }
