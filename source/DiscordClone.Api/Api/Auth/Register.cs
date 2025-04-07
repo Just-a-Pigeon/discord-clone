9641288d4b5d0d126f6f1999c977affffdab8ddd
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-07 12:01:08 +0200

    Add validation and documentation

--- source/DiscordClone.Api/Api/Auth/Register.cs
+++ source/DiscordClone.Api/Api/Auth/Register.cs
@@ -1,6 +1,10 @@
-﻿using DiscordClone.Contract.Rest.Request.Auth;
+﻿using System.Text.RegularExpressions;
+using DiscordClone.Api.Api.Account;
+using DiscordClone.Contract.Rest.Request.Auth;
+using DiscordClone.Contract.Rest.Response.Account;
 using DiscordClone.Domain.Entities.Consultation;
 using FastEndpoints;
+using FluentValidation;
 using Microsoft.AspNetCore.Identity;
 
 namespace DiscordClone.Api.Api.Auth;
@@ -37,5 +41,69 @@ public class Register(UserManager<ApplicationUser> userManager, SignInManager<Ap
         await userManager.AddToRoleAsync(user, "User");
 
         await SendOkAsync(ct);
+    }
+    
+    public class MyValidator : Validator<RegisterRequestDto>
+    {
+        public MyValidator()
+        {
+            RuleFor(x => x.Firstname)
+                .NotEmpty()
+                .WithMessage("Firstname is required")
+                .MaximumLength(100)
+                .WithMessage("Firstname cannot exceed 100 characters");
+
+            RuleFor(x => x.Lastname)
+                .NotEmpty()
+                .WithMessage("Lastname is required")
+                .MaximumLength(100)
+                .WithMessage("Lastname cannot exceed 100 characters");
+            
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Email must be a valid email address")
+                .MaximumLength(250)
+                .WithMessage("Email cannot exceed 250 characters");
+            
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("password is required")
+                .MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters")
+                .Must(p => Regex.IsMatch(p,"^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).*$"))
+                .WithMessage("Password must at least contain on upper case letters, numbers and special characters");
+            
+            RuleFor(x => x.Username)
+                .NotEmpty()
+                .WithMessage("Username is required")
+                .MaximumLength(100)
+                .WithMessage("Username cannot exceed 100 characters");
+                
+        }
+    }
+
+    public class Documentation : Summary<Register>
+    {
+        public Documentation()
+        {
+
+            Summary = "Creates a new account";
+            Description = "Creates a new account";
+            ExampleRequest = new RegisterRequestDto()
+            {
+               Firstname = "Pigeon",
+               Lastname = "city",
+               Email = "pigeon@gmail.com",
+               Password = "Pigeon!2",
+               Username = "ThePigeon",
+            };
+            Response(200, "Account was successfully created");
+
+            Response<ErrorResponse>(401,"Account already exists");
+            Response<ErrorResponse>(400,"Client side error");
+
+        }
     }
 }
