076a612aea7fd6962ba2e3273ff5fe4a259e9a8f
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-08 13:21:48 +0200

    Add SendDm endpoint

---
+++ source/DiscordClone.Api/Api/Message/Send/SendDm.cs
@@ -0,0 +1,34 @@
+﻿using DiscordClone.Api.Api.Binders;
+using DiscordClone.Contract.Rest.Request.Message;
+using DiscordClone.Persistence;
+
+using FastEndpoints;
+
+namespace DiscordClone.Api.Api.Message.Send;
+
+public class SendDm(DiscordCloneContext dbContext) : Endpoint<SendDm.Request>
+{
+    public override void Configure()
+    {
+        Post("dm");
+        Group<Send>();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var message = Domain.Entities.Consultation.Message.CreateDm(req.UserId,req.ReceiverId,req.Content,req.CreatedOn);
+        
+        dbContext.Add(message);
+        await dbContext.SaveChangesAsync(ct);
+
+        await SendOkAsync(ct);
+        
+    }
+
+
+    public class Request : SendRequestDto, IHasUserId
+    {
+        public Guid UserId { get; set; }
+    }
+    
+}
