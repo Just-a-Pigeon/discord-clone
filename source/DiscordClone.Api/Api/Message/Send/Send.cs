d788c9e5bbf512c764d58caa0ac24cc837b0d79b
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-08 13:21:23 +0200

    Add send group

---
+++ source/DiscordClone.Api/Api/Message/Send/Send.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+
+namespace DiscordClone.Api.Api.Message.Send;
+
+public class Send : Group
+{
+    public Send()
+    {
+        Configure("messages/send", ep =>
+        {
+            ep.Description(x =>
+
+            {
+                x.RequireAuthorization(); 
+                
+            });
+        });
+    }
+}
