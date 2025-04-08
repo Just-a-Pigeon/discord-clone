f16fac5beca0ad9f73afa12de28e99bf846566bc
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-08 15:53:41 +0200

    Add documentation and validation

--- source/DiscordClone.Api/Api/Message/Send/SendDm.cs
+++ source/DiscordClone.Api/Api/Message/Send/SendDm.cs
@@ -1,7 +1,9 @@
 ﻿using DiscordClone.Api.Api.Binders;
 using DiscordClone.Contract.Rest.Request.Message;
+using DiscordClone.Contract.Rest.Response.Message;
 using DiscordClone.Persistence;
 using FastEndpoints;
+using FluentValidation;
 
 namespace DiscordClone.Api.Api.Message.Send;
 
@@ -22,6 +24,45 @@ public class SendDm(DiscordCloneContext dbContext) : Endpoint<SendDm.Request>
         await dbContext.SaveChangesAsync(ct);
 
         await SendOkAsync(ct);
+    }
+
+    public class MyValidator : Validator<Request>
+    {
+        public MyValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UseId is required");
+
+            RuleFor(x => x.ReceiverId)
+                .NotEmpty()
+                .WithMessage("ReceiverId is required");
+
+            RuleFor(x => x.Content)
+                .NotEmpty()
+                .WithMessage("Content is required")
+                .MaximumLength(2000)
+                .WithMessage("Content is too long");
+        }
+    }
+
+    public class Documentation : Summary<SendDm>
+    {
+        public Documentation()
+        {
+            Summary = "Send message to the user dm";
+            Description = "Send message to the user dm";
+            ExampleRequest = new MessageResponseDto
+            {
+                Content = "This is a message send to the user dm",
+                CreatedOn = DateTimeOffset.Now,
+                SenderId = Guid.NewGuid()
+            };
+            Response(200, "message was sent to the user dm");
+
+            Response<ErrorResponse>(401, "cloud not send the message to user dm");
+            Response<ErrorResponse>(400, "Client side error");
+        }
     }
 
 
