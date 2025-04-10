8857a336d9035ee9b451707334015533f9d74d54
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-10 12:25:41 +0200

    Add Defaultname and Creategroup method #9

--- source/DiscordClone.Domain/Entities/Consultation/Group.cs
+++ source/DiscordClone.Domain/Entities/Consultation/Group.cs
@@ -1,10 +1,34 @@
-﻿namespace DiscordClone.Domain.Entities.Consultation;
+﻿using CSharpFunctionalExtensions;
+
+namespace DiscordClone.Domain.Entities.Consultation;
 
 public class Group
 {
-    public Guid GroupId { get; set; }
-    public Guid LeaderId { get; set; }
-    public string Name { get; set; }
-    public string ImagePath { get; set; }
-    public List<ApplicationUser> Members { get; set; }
+    private Group()
+    {
+    }
+
+    public Guid GroupId { get; private set; }
+    public Guid LeaderId { get; private set; }
+    public string Name { get; private set; }
+    public string ImagePath { get; private set; }
+    public List<ApplicationUser> Members { get; private set; }
+
+    private static string DefaultName(List<ApplicationUser> members)
+    {
+        var membersNames = members.Select(m => m.UserName);
+        return string.Join(", ", membersNames);
+    }
+
+    public static Result<Group, ValidationError> CreateGroup(Guid leaderId, string name, string imagePath,
+        List<ApplicationUser> members)
+    {
+        return new Group
+        {
+            LeaderId = leaderId,
+            Name = DefaultName(members),
+            ImagePath = imagePath,
+            Members = members
+        };
+    }
 }
