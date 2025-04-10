a40362274941076016d6619066941c0c470bd73b
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-10 11:41:28 +0200

    Add entitie group

---
+++ source/DiscordClone.Domain/Entities/Consultation/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace DiscordClone.Domain.Entities.Consultation;
+
+public class Group
+{
+    public Guid GroupId { get; set; }
+    public Guid LeaderId { get; set; }
+    public string Name { get; set; }
+    public string ImagePath { get; set; }
+    public List<ApplicationUser> Members { get; set; }
+}
