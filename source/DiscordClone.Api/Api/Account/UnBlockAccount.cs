ada9d0b846ca51e1740c63a350399bedba31b3e5
Jamy Van den Borre <jamy.vandenborre@hotmail.com>
2025-04-11 23:52:18 +0200

    Add feature to block people

---
+++ source/DiscordClone.Api/Api/Account/UnBlockAccount.cs
@@ -0,0 +1,78 @@
+﻿using DiscordClone.Api.Api.Binders;
+using DiscordClone.Persistence;
+using FastEndpoints;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordClone.Api.Api.Account;
+
+public class UnBlockAccount(DiscordCloneContext dbContext) : Endpoint<UnBlockAccount.Request>
+{
+    public override void Configure()
+    {
+        Get("{UnBlockUserId:Guid}/unblock-account");
+        Group<Accounts>();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var user = dbContext.Users
+            .Include(u => u.BlockedUsers)
+            .FirstOrDefault(u => u.Id == req.UserId);
+
+        if (user == null)
+        {
+            await SendErrorsAsync(cancellation: ct);
+            return;
+        }
+        
+        var unBlockUser = user.BlockedUsers.FirstOrDefault(u => u.Id == req.UnBlockUserId);
+
+        if (unBlockUser == null)
+        {
+            await SendOkAsync(cancellation: ct);
+            return;
+        }
+        
+        user.BlockedUsers.Remove(unBlockUser);
+        await dbContext.SaveChangesAsync(ct);
+
+        await SendOkAsync(ct);
+    }
+
+    public class Request : IHasUserId
+    {
+        public Guid UnBlockUserId { get; set; }
+        [HideFromDocs] public Guid UserId { get; set; }
+    }
+
+    public class MyValidator : Validator<Request>
+    {
+        public MyValidator()
+        {
+            RuleFor(x => x.UnBlockUserId)
+                .NotEmpty()
+                .WithMessage("UnBlockUserId is required");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("UserId is required")
+                .NotEqual(x => x.UnBlockUserId)
+                .WithMessage("You cannot unblock yourself");
+        }
+    }
+
+    public class Documentation : Summary<UnBlockAccount>
+    {
+        public Documentation()
+        {
+            Summary = "Unblock a user by Id";
+            Description = "Unblock a user by Idd";
+            ExampleRequest = new Request
+            {
+                UnBlockUserId = Guid.NewGuid()
+            };
+            Response(200, "User is blocked");
+        }
+    }
+}
---
+++ source/DiscordClone.Persistence/Migrations/20250411211618_AddBlockedListToUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordClone.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBlockedListToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "application_user_id",
+                table: "AspNetUsers",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_asp_net_users_application_user_id",
+                table: "AspNetUsers",
+                column: "application_user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "fk_asp_net_users_asp_net_users_application_user_id",
+                table: "AspNetUsers",
+                column: "application_user_id",
+                principalTable: "AspNetUsers",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "fk_asp_net_users_asp_net_users_application_user_id",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "ix_asp_net_users_application_user_id",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "application_user_id",
+                table: "AspNetUsers");
+        }
+    }
+}
