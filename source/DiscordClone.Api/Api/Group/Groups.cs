5027e7b6e4d919437344a37233ec466f5393f463
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-11 15:01:04 +0200

    Add groups endpoint #9

--- source/DiscordClone.Api/Api/Group/Groups.cs
+++ source/DiscordClone.Api/Api/Group/Groups.cs
@@ -1,6 +1,14 @@
 ﻿namespace DiscordClone.Api.Api.Group;
 
-public class Groups:
+public class Groups : FastEndpoints.Group
 {
-    
+    public Groups()
+    {
+        Configure("groups", ep =>
+        {
+            {
+                ep.Description(x => { x.RequireAuthorization(); });
+            }
+        });
+    }
 }
