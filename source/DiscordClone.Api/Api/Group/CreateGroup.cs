1c51d84889da2687e2aa3b1b2901142980d370b0
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-11 15:01:21 +0200

    Add CreateGroup endpoint #9

---
+++ source/DiscordClone.Api/Api/Group/CreateGroup.cs
@@ -0,0 +1,32 @@
+﻿using DiscordClone.Api.Api.Binders;
+using DiscordClone.Persistence;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using DomainGroup = DiscordClone.Domain.Entities.Consultation;
+
+namespace DiscordClone.Api.Api.Group;
+
+public class CreateGroup(DiscordCloneContext dbContext) : Endpoint<CreateGroup.Request>
+{
+    public override void Configure()
+    {
+        Post("Create");
+        Group<Groups>();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var members = await dbContext.Users.Where(u => req.MembersId.Contains(u.Id)).ToListAsync(ct);
+        var group = DomainGroup.Group.CreateGroup(req.UserId, members);
+
+        dbContext.Add(group);
+        await dbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+
+    public class Request : IHasUserId
+    {
+        public List<Guid> MembersId { get; set; }
+        [HideFromDocs] public Guid UserId { get; set; }
+    }
+}
