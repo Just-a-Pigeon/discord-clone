551c26992df577bbb5433691a48a559899c450ff
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-13 12:45:16 +0200

    Format document

--- source/DiscordClone.Api/Api/Message/Messages.cs
+++ source/DiscordClone.Api/Api/Message/Messages.cs
@@ -1,18 +1,10 @@
-﻿using FastEndpoints;
-
-namespace DiscordClone.Api.Api.Message;
+﻿namespace DiscordClone.Api.Api.Message;
 
 public class Messages : FastEndpoints.Group
 {
     public Messages()
     {
         // ReSharper disable once VirtualMemberCallInConstructor
-        Configure("messages", ep =>
-        {
-            ep.Description(x =>
-            {
-                x.RequireAuthorization();
-            });
-        });
+        Configure("messages", ep => { ep.Description(x => { x.RequireAuthorization(); }); });
     }
 }
