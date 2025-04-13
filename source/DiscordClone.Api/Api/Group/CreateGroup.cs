321934047511f4fc2566275feca12907c3c1bb4c
yanickHauwelaert <hauwelaert.yanick@gmail.com>
2025-04-13 12:45:06 +0200

    Add Validation

--- source/DiscordClone.Api/Api/Group/CreateGroup.cs
+++ source/DiscordClone.Api/Api/Group/CreateGroup.cs
@@ -19,6 +19,12 @@ public class CreateGroup(DiscordCloneContext dbContext) : Endpoint<CreateGroup.R
         var members = await dbContext.Users.Where(u => req.MembersId.Contains(u.Id)).ToListAsync(ct);
         var group = DomainGroup.Group.CreateGroup(req.UserId, members);
 
+        if (members.Exists(u => u.Id == req.UserId))
+        {
+            var leader = dbContext.Users.FirstOrDefault(u => u.Id == req.UserId);
+            members.Add(leader);
+        }
+        
         dbContext.Add(group);
         await dbContext.SaveChangesAsync(ct);
         await SendOkAsync(ct);
