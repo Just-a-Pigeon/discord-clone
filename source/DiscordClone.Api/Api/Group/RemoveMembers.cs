47bc2ce0759b1a6609c906d3ef0e378558ba7a09
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2025-04-14 13:51:11 +0200

    Add documentation and Validation

--- source/DiscordClone.Api/Api/Group/RemoveMembers.cs
+++ source/DiscordClone.Api/Api/Group/RemoveMembers.cs
@@ -1,11 +1,12 @@
 ﻿using DiscordClone.Api.Api.Binders;
 using DiscordClone.Persistence;
 using FastEndpoints;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 
 namespace DiscordClone.Api.Api.Group;
 
-public class RemoveMembers(DiscordCloneContext dbContext): Endpoint<RemoveMembers.Request>
+public class RemoveMembers(DiscordCloneContext dbContext) : Endpoint<RemoveMembers.Request>
 {
     public override void Configure()
     {
@@ -15,7 +16,8 @@ public class RemoveMembers(DiscordCloneContext dbContext): Endpoint<RemoveMember
 
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        var group = await dbContext.Groups.Include(group => group.Members).SingleOrDefaultAsync(g => g.Id == req.GroupId,ct);
+        var group = await dbContext.Groups.Include(group => group.Members)
+            .SingleOrDefaultAsync(g => g.Id == req.GroupId, ct);
 
         if (group == null)
         {
@@ -30,18 +32,55 @@ public class RemoveMembers(DiscordCloneContext dbContext): Endpoint<RemoveMember
         }
 
         var member = group.Members.SingleOrDefault(m => m.Id == req.MemberId);
-        
-        if(member is not null)
+
+        if (member is not null)
             group.Members.Remove(member);
 
         await dbContext.SaveChangesAsync(ct);
         await SendOkAsync(ct);
     }
 
-    public class Request: IHasUserId
+    public class Request : IHasUserId
     {
         [HideFromDocs] public Guid UserId { get; set; }
         [HideFromDocs] public Guid MemberId { get; set; }
         public Guid GroupId { get; set; }
+    }
+
+    public class MyValidator : Validator<RemoveMembers.Request>
+    {
+        public MyValidator()
+        {
+            RuleFor(x => x.GroupId)
+                .NotEmpty()
+                .WithMessage("RoomId is required");
+
+            RuleFor(x => x.UserId)
+                .NotEmpty()
+                .WithMessage("userId is required");
+
+            RuleFor(x => x.MemberId)
+                .NotEmpty()
+                .WithMessage("MemberId is required");
+        }
+    }
+
+    public class Documentation : Summary<RemoveMembers>
+    {
+        public Documentation()
+        {
+            Summary = "Remove a member of the group";
+            Description = "Remove a member of the group";
+            ExampleRequest = new RemoveMembers.Request
+            {
+                GroupId = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                MemberId = Guid.NewGuid()
+            };
+            Response(200, "Removed member of the group");
+
+            Response<ErrorResponse>(401, "couldn`t remove the member of the group");
+            Response<ErrorResponse>(400, "Client side error");
+        }
     }
 }
