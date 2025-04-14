5a74a4ef3bfc0109a18056557344ff59fc4df303
yanickHauwelaert <yanick.hauwelaert@student.howest.be>
2025-04-14 13:43:10 +0200

    Add RemoveMembers endpoint #17

---
+++ source/DiscordClone.Api/Api/Group/RemoveMembers.cs
@@ -0,0 +1,47 @@
+﻿using DiscordClone.Api.Api.Binders;
+using DiscordClone.Persistence;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordClone.Api.Api.Group;
+
+public class RemoveMembers(DiscordCloneContext dbContext): Endpoint<RemoveMembers.Request>
+{
+    public override void Configure()
+    {
+        Put("{GroupId:guid}/remove/{MemberId:guid}");
+        Group<Groups>();
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var group = await dbContext.Groups.Include(group => group.Members).SingleOrDefaultAsync(g => g.Id == req.GroupId,ct);
+
+        if (group == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        if (group.LeaderId != req.UserId)
+        {
+            await SendUnauthorizedAsync(ct);
+            return;
+        }
+
+        var member = group.Members.SingleOrDefault(m => m.Id == req.MemberId);
+        
+        if(member is not null)
+            group.Members.Remove(member);
+
+        await dbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+
+    public class Request: IHasUserId
+    {
+        [HideFromDocs] public Guid UserId { get; set; }
+        [HideFromDocs] public Guid MemberId { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
